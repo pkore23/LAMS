b4f29cfcbbf7ac667c77d490288c238b0c4620ab
pkore23 <pkore23@gmail.com>
2016-02-02 19:24:03 +0530

    .

--- app/src/main/java/com/beproject/lams/LoginActivityFragment.java
+++ app/src/main/java/com/beproject/lams/LoginActivityFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -15,6 +16,9 @@ import android.widget.Button;
 public class LoginActivityFragment extends Fragment implements View.OnClickListener {
     View rootView;
     Button b;
+    EditText p,u;
+    private String DUMMY_CREDENTIALS[] = {"admin:admin"};
+    private int loginAttempt;
     public LoginActivityFragment() {
     }
 
@@ -22,6 +26,9 @@ public class LoginActivityFragment extends Fragment implements View.OnClickListe
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        loginAttempt = 0;
+        p = (EditText) rootView.findViewById(R.id.passd);
+        u = (EditText) rootView.findViewById(R.id.usrid);
         b = (Button) rootView.findViewById(R.id.btnlogin);
         b.setOnClickListener(this);
         return rootView;
@@ -29,7 +36,29 @@ public class LoginActivityFragment extends Fragment implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Intent i = new Intent(getContext(),OptionsActivity.class);
-        startActivity(i);
+        if(attemptLogin()) {
+            Intent i = new Intent(getContext(), OptionsActivity.class);
+            startActivity(i);
+        }
+        else if(loginAttempt<3){
+            Snackbar.make(v,"Login attemt faild: Incorrect credentials.",Snackbar.LENGTH_SHORT).show();
+            ++loginAttempt;
+            p.setText("");
+            u.setText("");
+            u.requestFocus();
+        }
+        else {
+
+        }
+    }
+
+    private boolean attemptLogin() {
+        for(String credentials:DUMMY_CREDENTIALS){
+            String cr[] = credentials.split(":");
+            if((u.getText().toString()).equals(cr[0])){
+                return p.getText().toString().equals(cr[1]);
+            }
+        }
+        return false;
     }
 }
