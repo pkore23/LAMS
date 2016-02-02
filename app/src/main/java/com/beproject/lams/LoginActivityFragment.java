f009986749ddcb58a186a6f41a956e110cd428b8
pkore23 <pkore23@gmail.com>
2016-02-02 18:27:07 +0530

    modified:   .idea/misc.xml 	modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/com/beproject/lams/LoginActivityFragment.java 	new file:   app/src/main/java/com/beproject/lams/OptionsActivity.java 	new file:   app/src/main/java/com/beproject/lams/OptionsActivityFragment.java 	new file:   app/src/main/res/drawable-hdpi/ic_action_name.png 	new file:   app/src/main/res/drawable-mdpi/ic_action_name.png 	new file:   app/src/main/res/drawable-xhdpi/ic_action_name.png 	new file:   app/src/main/res/drawable-xxhdpi/ic_action_name.png 	new file:   app/src/main/res/layout/activity_options.xml 	new file:   app/src/main/res/layout/content_options.xml 	new file:   app/src/main/res/layout/fragment_options.xml 	new file:   app/src/main/res/menu/menu_options.xml 	modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/com/beproject/lams/LoginActivityFragment.java
+++ app/src/main/java/com/beproject/lams/LoginActivityFragment.java
@@ -1,22 +1,35 @@
 package com.beproject.lams;
 
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class LoginActivityFragment extends Fragment {
-
+public class LoginActivityFragment extends Fragment implements View.OnClickListener {
+    View rootView;
+    Button b;
     public LoginActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        b = (Button) rootView.findViewById(R.id.btnlogin);
+        b.setOnClickListener(this);
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(getContext(),OptionsActivity.class);
+        startActivity(i);
     }
 }
